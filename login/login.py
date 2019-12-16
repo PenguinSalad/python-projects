42befdf3ed37a4ea9ce92de8d741a65e1df89d83
PenguinSalad <rogerdiazcruells@outlook.com>
2019-12-16 16:19:47 +0100

    Deleted test files and added hashing for more security

--- login/login.py
+++ login/login.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import time
-
+from hashlib import blake2b
+from hmac import compare_digest
 
 def welcome():
     print ("-"*75)
@@ -9,60 +10,64 @@ def welcome():
     time.sleep(1)
     print ("\nWould you like to LOGIN or REGISTER?\n")
 
+main = True
+wrongPass = 0
 #Main while loop
-while True:
+while main:
     #Welcome message
     welcome()
     asking = True
+    access = False
     while asking:
-        regorLog = input('"Press l for Login, r for Register "')
+        regorLog = input('"Press l for Login, r for Register" ')
         if (regorLog == "r") or (regorLog == "R"):
         #If Register
             #Ask for username and password
             #Username loop
             #If username already exists, ask for another one
-            username = input("Username: ")
+            username = input("user: ").encode('utf-8')
             #Make the user repeat the password
             while True:
-                password = input("Password: ")
-                repPass = input("Please repeat your password: ")
+                password = input("password: ").encode('utf-8')
+                repPass = input("Please repeat your password: ").encode('utf-8')
                 if (password == repPass):
-                    #Store those 2 values in a .txt
+                    userInfo = blake2b(digest_size=20)
+                    userInfo.update(b"%s-%s" % (username, password))
+                    #Open users file
+                    usersFile = open('users.txt', 'a')
+                    usersFile.write(userInfo.hexdigest()) #Store the hashed userInfo
+                    usersFile.write("\n")
+                    usersFile.close()
+                    #User created!
                     print("\nUser %s created successfuly! Going back to menu...\n" % username)
-                    time.sleep(2.5)
-                    user = ("%s-%s\n" % (username, password))
-                    with open("users.txt", "a") as usersfile:
-                        usersfile.write(user)
-                        usersfile.close()
+                    time.sleep(1)
                     break
                 else:
                     print("Passwords don't match, please try again")
             asking = False
-
-
-
         elif (regorLog == "l") or (regorLog == "L"):
         #Elif Login
             #Ask for the username and password
-
-            #Checks if the username exists
+            logUser = input("Username: ").encode('utf-8')
+            logPass = input("Password: ").encode('utf-8')
+            #Hashes the login info
+            loginInfo = blake2b(digest_size=20)
+            loginInfo.update(b"%s-%s" % (logUser, logPass))
+            #Reads the users.txt file, line per line
             with open("users.txt") as usersfile:
-                logUser = input("Username: ")
                 readLine = usersfile.readlines()
-
-                for x in range(len(readLine)):
-                    if (logUser == readLine[x].split("-")[0]):
-                        print("Ok")
-                        while True:
-                            logPass = input("Password: ")
-                            #Checks if the password matches the username selected
-                            if (logPass == readLine[x].rstrip().split("-")[1]):
-                                #Gets access
-                                print("UEUEUEUEUUEUE")
-                                break
-                            else:
-                                print("Incorrect password, try again")
-
-            break
-        else:
+            for x in range(len(readLine)):
+                #If one of the lines contains the same hash as the one used for login:
+                if (loginInfo.hexdigest() == readLine[x].rstrip()):
+                    #Grants access
+                    access = True
+                    break
+            else: #If the hash doesn't exist
+                print("Wrong username or password\n")
+        else: #In case they don't write either L or R
             print("That's not a valid answer\n")
+
+        if access: #If the user and password match
+            print("I'm in")
+            main = False
+            asking = False
